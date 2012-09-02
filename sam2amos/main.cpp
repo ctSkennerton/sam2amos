68b49520978d9489385ac4b5dc261e4c8b537bd7
ctSkennerton <c.skennerton@gmail.com>
2012-09-03 00:00:23 +1000

    Initial Commit

---
+++ sam2amos/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  sam2amos
+//
+//  Created by Connor Skennerton on 3/09/12.
+//  Copyright (c) 2012 Australian Centre for Ecogenomics. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
